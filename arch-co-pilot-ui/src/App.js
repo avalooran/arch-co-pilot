7655ca75ea844eb36d727ef293a625968c0823f9
avalooran <avalooran@devx.systems>
2024-09-13 20:30:34 +0530

    Typewriting Effect

--- arch-co-pilot-ui/src/App.js
+++ arch-co-pilot-ui/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Header from './components/Header';
 import Chat from './components/Chat';
 import ChatInput from './components/ChatInput';
@@ -7,15 +7,34 @@ import { chatItemsMock } from './constants/mock';
 
 function App() {
   const [chatItems, updateChatItems] = useState(chatItemsMock);
+  const chatItemsRef = useRef(chatItems);
+  chatItemsRef.current = chatItems;
   const onSearch = (searchText) => {
-    const searchedText = searchText.trim();
-    updateChatItems([...chatItems, {
-      message: searchedText,
+    const searchedInput = searchText.trim();
+    updateChatItems([...chatItemsRef.current, {
+      message: searchedInput,
       isBot: false
-    }]);
+    }]); 
+    insertBotsResponse("The substring() method of String values returns the part of this string from the start index up to and excluding the end index, or to the end of the string if no end index is supplied.");
+  }  
+  const insertBotsResponse = (responseMsg) => {
     setTimeout(() => {
-
-    }, 1000);
+      updateChatItems([...chatItemsRef.current, {
+        message: "",
+        isBot: true
+      }]);        
+    }, 10);
+    for(let i=0; i < responseMsg.length; i++) {      
+      const answr = responseMsg.charAt(i);
+      setTimeout(() => {
+         updateChatItems(chatItemsRef.current.map((x, ind) => {
+            if(ind == chatItemsRef.current.length - 1)
+              return { message: responseMsg.substring(0, i + 1), isBot: true }
+            else 
+              return x;
+        }));
+      },1000 + (i * 2));
+    }    
   }
   return (
     <div className="App">
