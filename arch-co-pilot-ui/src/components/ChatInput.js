69602f6be2d811d3c2c5d02588b0ace2bb868fd8
Allan <avalooran@devx.systems>
2024-09-19 15:21:31 +0530

    SearchAndUploadIconsPlaced

--- arch-co-pilot-ui/src/components/ChatInput.js
+++ arch-co-pilot-ui/src/components/ChatInput.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './ChatInput.css';
+import { BsFillSendFill } from "react-icons/bs";
+import { FaFileImage } from "react-icons/fa";
 
 function ChatInput({ onSearch, disableTextArea }) {
     const [searchText, updateSearchText] = useState("");
     const [canAsk, updateCanAsk] = useState(false);
     const onKeyDown = (e) => {
-        if (e.key == "Enter" && !disableTextArea) {
+        if (e.key === "Enter" && !disableTextArea) {
             e.preventDefault();
             onAsk();
             updateSearchText("");
@@ -13,20 +15,45 @@ function ChatInput({ onSearch, disableTextArea }) {
     }
     const onSearchTextChange = (e) => {
         const input = e.target.value;
-        updateCanAsk(input.trim() != "");
+        updateCanAsk(input.trim() !== "");
         updateSearchText(input);
     }
     const onAsk = () => {
         if (!disableTextArea) {
-            if (searchText.trim() != "")
+            if (searchText.trim() !== "")
                 onSearch(searchText);
             updateSearchText("");
         }
     }
     return (
         <div className="chat-input">
+            <div>
+                <FaFileImage
+                    size={30}
+                    style={{
+                        ...(!(canAsk && !disableTextArea) && {
+                            cursor: "not-allowed",
+                            fill: "gray"
+                        })
+                    }}
+                />
+            </div>
             <textarea value={searchText} onChange={onSearchTextChange} onKeyDown={onKeyDown} />
-            <button className={canAsk && !disableTextArea ? "active" : ""} disabled={!(canAsk && !disableTextArea)} onClick={onAsk}>Ask</button>
+            <div>
+                <BsFillSendFill
+                    size={30}
+                    style={{
+                        ...(!(canAsk && !disableTextArea) && {
+                            cursor: "not-allowed",
+                            fill: "gray"
+                        })
+                    }}
+                    onClick={onAsk}
+                />
+
+            </div>
+
+            {/* <button className={canAsk && !disableTextArea ? "active" : ""} disabled={!(canAsk && !disableTextArea)} onClick={onAsk}>Ask</button> */}
         </div>
     );
 }
